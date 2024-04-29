84c68ebe664d708420e7be66e4546a9276336e09
sadafwalliyani <sadafwalliyani@gmail.com>
2024-04-29 10:22:22 +0500

    add

--- Chatbots/voice chatbot/text_to_speech.py
+++
@@ -1,9 +0,0 @@
-
-from transformers import pipeline
-
-tts_pipeline = pipeline("text-to-speech", model="facebook/fastspeech2-en-ljspeech")
-
-def synthesize_speech(text):
-    speech = tts_pipeline(text)
-    audio_data = speech[0]['file']
-    return audio_data
